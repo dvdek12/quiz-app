3b9ebd3b230fa3acc16f25964b6b59daa5c2bcce
dvdek12 <przemekdudek321@wp.pl>
2024-01-15 21:20:39 +0100

    added 404 page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+    <NuxtLayout name="404">
+        <span class="text-white text-xl ">
+            Strona: <i>{{ fullPath }}</i> nie istnieje
+        </span>
+        <NuxtLink to="/" class="text-blue-400 underline text-xl ">
+            Wróć
+        </NuxtLink>
+    </NuxtLayout>
+</template>
+
+<script setup>
+const { fullPath } = useRoute()
+</script>
---
+++ layouts/404.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-screen h-screen bg-gray-700 flex flex-col items-center justify-center space-y-6">
+        <div class="bg-yellow-400 py-3 px-6 mt-2 w-1/4 rounded-lg">
+            <h2 class="text-center text-4xl font-semibold text-black">
+                PAGE NOT FOUND
+            </h2>
+        </div>
+
+        <div class="w-1/2  text-center">
+            <slot/>
+        </div>
+    </div>
+</template>
