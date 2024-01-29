bf29930557e121b0505a73944782ca06154b7e05
dvdek12 <przemekdudek321@wp.pl>
2024-01-30 00:01:55 +0100

    addded header component

---
+++ components/Navigation/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="w-full ">
+        <nav class="w-full flex fixed top-0 bg-cordovan py-6 bangers">
+            <div class="w-1/2 ">
+                <h2 class="text-5xl font-white text-isabelline tracking-wide ml-10">
+                    Quiz Master
+                </h2>
+            </div>
+            <div class="w-1/2">
+                <NuxtLink to="/login">
+                    Login
+                </NuxtLink>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style>
+.bangers {
+    font-family: 'Bangers';
+}
+</style>
