905ecd011213d620a9f38463d7775e4e3065301b
dvdek12 <przemekdudek321@wp.pl>
2024-02-07 18:43:27 +0100

    Modified design forms

--- pages/register.vue
+++ pages/register.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="flex  flex-col justify-center px-6 py-12 lg:px-8  rounded-md">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-cordovan">Sign in to your
-                account
+            <h2 class="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-cordovan">Sign up new account
             </h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form class="space-y-6" @submit.prevent="signInWithEmail()">
+            <form class="space-y-6" @submit.prevent="signUpNewUser()">
                 <div>
                     <label for="email" class="block text-lg font-medium leading-6 text-cordovan/80">Email address</label>
                     <div class="mt-2">
@@ -28,55 +27,98 @@
                 </div>
 
                 <div>
+                    <div class="flex items-center justify-between">
+                        <label for="password" class="block text-lg font-medium leading-6 text-cordovan/80">Repeat
+                            password</label>
+                    </div>
+                    <div class="mt-2">
+                        <input id="password" v-model="password2" name="password" type="password"
+                            autocomplete="current-password" required
+                            class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-cordovan sm:text-md sm:leading-6 transition-all duration-300 ease-in-out">
+                    </div>
+                </div>
+
+                <div>
                     <button type="submit"
                         class="flex w-full justify-center rounded-md bg-cordovan px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-all duration-300 ease-in-out">
-                        Sign in
+                        Sign up
                     </button>
                 </div>
             </form>
 
             <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                <NuxtLink to="/register"
+                Already have account?
+                <NuxtLink to="/login"
                     class="font-semibold leading-6 text-cordovan hover:text-red-900 transition-all duration-300 ease-in-out">
-                    Sign up now!</NuxtLink>
+                    Sign in now!</NuxtLink>
             </p>
         </div>
     </div>
+    <!-- <form class="flex flex-col" @submit.prevent="signUpNewUser()">
+        <h2 class="text-isabelline text-5xl mb-12">Zarejestruj się</h2>
+
+        <div class="flex flex-col space-y-2">
+            <span>
+                Email
+            </span>
+            <input type="email" v-model="email">
+        </div>
+
+        <div class="flex flex-col space-y-2">
+            <span>
+                Hasło
+            </span>
+            <input type="password" v-model="password">
+        </div>
+
+        {{ errorMsg }}
+        {{ successMsg }}
+
+        <input type="submit">
+    </form> -->
 </template>
 
 
 <script setup>
 const supabase = useSupabaseClient()
-const router = useRouter()
 
 const email = ref("")
 const password = ref(null)
+const password2 = ref(null)
 const errorMsg = ref(null)
+const successMsg = ref(null)
 
-async function signInWithEmail() {
+async function signUpNewUser() {
     try {
-        const { data, error } = await supabase.auth.signInWithPassword({
+        const { data, error } = await supabase.auth.signUp({
             email: email.value,
-            password: password.value
-        })
+            password: password.value,
+        });
 
         console.log(data);
 
         if (error) throw error
-        router.push("/")
+
+        successMsg.value = "Check your email to confirm your account!"
+
     } catch (e) {
         errorMsg.value = e.message
     }
-}
+    // const { data, error } = await supabase.auth.signUp({
+    //     email: email.value,
+    //     password: password.value,
+    //     options: {
+    //         emailRedirectTo: 'http://localhost:3000/'
+    //     }
+    // })
 
+    // if (error) throw error
+
+    // console.log(data);
+}
 
 definePageMeta({
     layout: 'auth'
-})
-
-useHead({
-    title: 'Login Page'
 })
 
 </script>
